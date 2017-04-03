75991ba5df0414c52413c86c8e9ad5426351ee3d
MazdaX <mazdak.salavati@gmail.com>
2017-04-03 11:38:17 +0100

    MD5 summary checker for FASTQ files

---
+++ MD5Sum_Checker.sh
@@ -0,0 +1,18 @@
+##==============================================##
+##              Md5sum checker                  ##
+##              by                              ##
+##              Dr.Mazdak Salavati              ##
+##              30.03.2017                      ##
+##==============================================##
+#!/bin/bash
+for f in ./*/*/*.fastq.gz
+do
+key="$(md5sum $f|awk '{print$1}')"
+        if grep -q $key $f.md5 ; then
+                echo "Passed"
+        else
+                echo "$f failed md5sum check!"
+        fi
+
+done;
+
