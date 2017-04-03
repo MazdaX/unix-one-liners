426d8fd984fd489821a1f9e11769ed1c24897816
MazdaX <mazdak.salavati@gmail.com>
2017-04-03 11:57:26 +0100

    clean up 2

--- merger_fastq.sh
+++ merger_fastq.sh
@@ -5,7 +5,10 @@
 ##              Dr.Mazdak Salavati              ##
 ##              29.03.2017                      ##
 ##==============================================##
-#This is a solution to when you have the same library sequenced multiple times and you have to merge them. Bare in mind the files should share a commong sample name to be used as a substring in find. There is no need to zcat either as bgzip compression handles the output as it was: two .gz files glued to eachother file after another.
+# This is a solution to when you have the same library sequenced multiple times and you have to merge them. 
+# Bare in mind the files should share a commong sample name to be used as a substring in find. 
+# There is no need to zcat either as bgzip compression handles the output as it was: 
+# Two .gz files glued to eachother file after another.
 
 mkdir -p ./Merged;
 for f in "./"*.fastq.gz;
