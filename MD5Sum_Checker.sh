1590323b217fb6529fa667faa9afb6441d38b8b2
MazdaX <MazdaX@users.noreply.github.com>
2017-04-03 13:21:11 +0100

    Update MD5Sum_Checker.sh

--- MD5Sum_Checker.sh
+++ MD5Sum_Checker.sh
@@ -5,13 +5,14 @@
 ##              Dr.Mazdak Salavati              ##
 ##              30.03.2017                      ##
 ##==============================================##
-for f in ./*/*/*.fastq.gz
+for f in ./*/*/*.fastq.gz #Just in case your files live in many subfolders
 do
 key="$(md5sum $f|awk '{print$1}')"
         if grep -q $key $f.md5 ; then
                 echo "Passed"
         else
-                echo "$f failed md5sum check!"
+                echo "$f failed md5sum check!" #output is to terminal but could always be appended to a file >> fail_report.txt
+                echo "$f" >> fial_report.txt
         fi
 
 done;
