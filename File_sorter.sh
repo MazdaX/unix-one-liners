f723d9c581eccdad5e5e234512d0860dceffcf07
MazdaX <mazdak.salavati@gmail.com>
2017-04-04 15:25:57 +0100

    File sorter PDF template

---
+++ File_sorter.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+##==============================================##
+##              File Sorter	                ##
+##              by                              ##
+##              Dr.Mazdak Salavati              ##
+##              16.03.2017                      ##
+##==============================================##
+## Have you ever had to sort out 1000s of files in to few groups (viz. folders)?!
+## I had more than 1000 PDF files named with individual IDs that only existed on a separate list ('ID.list' was of comma separated values).
+## ID.list also had GROUP information under 1st column ($1) and individual IDs under 4th ($4). 
+
+## Destination folders 
+
+mkdir -p ./ABC/GROUP1
+mkdir -p ./ABC/GROUP2
+mkdir -p ./ABC/GROUP3 
+mkdir -p ./ABC/GROUP4
+
+## BASH indexes from 1 and lefthand side excluded (Similar to python).
+
+for pdf in "./ABC/"*.pdf
+do 
+	awk -F',' '($1 == "GROUP1"){print $4}' ./ID.list > temp1
+	if grep ${pdf:39:18} temp1; #${pdf:39:18} This coordinate was taking the ID part of my PDF file names. Adjust for your own accordingly.
+		then
+		mv $pdf ./ABC/GROUP1/${pdf:39:22} #PDF file full name excl. pevious PWD
+	fi;
+	awk -F',' '($1 == "GROUP2"){print $4}' ./ID.list > temp2
+	if grep ${pdf:39:18} temp2; 
+		then
+		mv $pdf ./ABC/GROUP2/${pdf:39:22} 
+	fi;
+	awk -F',' '($1 == "GROUP3"){print $4}' ./ID.list > temp3
+	if grep ${pdf:39:18} temp3; 
+		then
+		mv $pdf ./ABC/GROUP3/${pdf:39:22} 
+	fi;
+	awk -F',' '($1 == "GROUP4"){print $4}' ./ID.list > temp4
+	if grep ${pdf:39:18} temp4; 
+		then
+		mv $pdf ./ABC/GROUP4/${pdf:39:22} 
+	fi;
+done
+
+## Clean up (vey conservative to accomodate for different shells)
+
+rm -f temp1
+rm -f temp2
+rm -f temp3
+rm -f temp4
+
