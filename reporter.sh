558f3d86f2dfc53579d6e4b4ad663d7caddd21ee
Mazdak Salavati <MazdaX@users.noreply.github.com>
2019-01-22 12:33:29 +0000

    Create reporter.sh
    The reporter script for SunGrid Engine scheduler (probably works for PBS based HPCs as well)

---
+++ reporter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Reported script for SGE scheduler
+while true;do
+        qstat -ext -u $USER
+        tag=$(qstat -ext -u $USER | awk 'NR==3 {print $1}')
+        qstat -j $tag | grep 'usage'
+        printf  '\n%s\n' "Press Ctrl+C to stop the reporter!"
+        sleep 5
+done;
