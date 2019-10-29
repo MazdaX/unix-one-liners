e2b7a739d3836c7b2beeb454905450247ebe45b1
Mazdax <mazdak.salavati@gmail.com>
2019-10-29 13:48:57 +0000

    the stepping argument added

--- int_maker.sh
+++ int_maker.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
-echo "Please provide chromosome name and lenth e.g chr1 275000000"
-read chr length
-start=1000000
+echo "Please provide chromosome name, length and step size e.g chr1 275000000 5000000"
+read chr length step
+start=$step
 printf '%s:%d-%s\n' "$chr" "1" "${start}" > ${chr}_intervals.list
 while [ "$start" -lt "$length" ];do
-	chunk=$(($start+1000000))
-	while [ "$chunk" -lt "$length" ];do  
+	        chunk=$(($start+$step))
+	while [ "$chunk" -lt "$length" ];do
 		printf '%s:%d-%s\n' "$chr" "${start}" "${chunk}" >> ${chr}_intervals.list
 		start=$chunk
-		chunk=$(($chunk+1000000))
-		if [ "$chunk" -ge "$length" ];then 
+		chunk=$(($chunk+$step))
+		if [ "$chunk" -ge "$length" ];then
 			printf '%s:%d-%s\n' "$chr" "${start}" "${length}" >> ${chr}_intervals.list
 			break
 		fi
 	done;
 	break
 done;
-
-
-	
-
 
